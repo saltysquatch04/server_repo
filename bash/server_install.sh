64ddc5a7be77f554a09a98bb887d2b6d33394699
Aedan <aedanl802@gmail.com>
2023-12-22 13:52:00 -0500

    updated install script, testing for mc only

---
+++ bash/server_install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# #making directories
+# cd /
+
+# #media_server directories
+# mkdir media_server
+# mkdir /media_server/appdata
+# mkdir /media_server/appdata/config
+# mkdir /media_server/downloads
+# mkdir /media_server/media
+# mkdir /media_server/media/movies
+# mkdir /media_server/media/tv
+
+#mc directories
+mkdir mc_server
+mkdir mc_server/data/mods
+mkdir mc_server/data/config
+
+#create docker containers
+cd ~
+# docker compose -f "server_repo/docker/portainer.yml" -f "server_repo/docker/media_server.yml" up -d
+docker compose -f "server_repo/docker/mc_server.yml" up -d
+
