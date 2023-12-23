521aabd47c453fb538cd269755a8a1d3a2dd7690
Aedan <aedanl802@gmail.com>
2023-12-23 17:28:50 -0500

    testing media server

--- bash/server_install.sh
+++ bash/server_install.sh
@@ -3,31 +3,29 @@
 #moving to root
 cd /
 
-# #media_server directories
-# mkdir media_server
-# mkdir /media_server/appdata
-# mkdir /media_server/appdata/config
-# mkdir /media_server/downloads
-# mkdir /media_server/media
-# mkdir /media_server/media/movies
-# mkdir /media_server/media/tv
+#media_server directories
+mkdir media_server
+mkdir /media_server/appdata
+mkdir /media_server/appdata/config
+mkdir /media_server/downloads
+mkdir /media_server/media
+mkdir /media_server/media/movies
+mkdir /media_server/media/tv
 
 #mc directories
-mkdir mc_server
-mkdir mc_server/data
-# mkdir /mc_server/data/mods
-# mkdir /mc_server/data/config
+# mkdir mc_server
+# mkdir mc_server/data
 
-#create docker containers
-# docker volume create portainer_data
-# docker run -d -p 9000:9000 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
-# # docker compose -f "/home/aedan/server_repo/docker/media_server.yml" up -d
-docker compose -f "/home/aedan/server_repo/docker/mc_server.yml" up -d
+create docker containers
+docker volume create portainer_data
+docker run -d -p 9000:9000 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+docker compose -f "/home/aedan/server_repo/docker/media_server.yml" up -d
+# docker compose -f "/home/aedan/server_repo/docker/mc_server.yml" up -d
 
-echo "Waiting..."
-sleep 120
+# echo "Waiting..."
+# sleep 120
 
 #copying mods to correct directory
-cp /home/aedan/server_repo/mods/* /mc_server/data/mods
+# cp /home/aedan/server_repo/mods/* /mc_server/data/mods
 
 echo "Setup Complete."
