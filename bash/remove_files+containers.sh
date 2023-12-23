02b2c9089cc61320a2c38c6bcd6d33f51922da07
Aedan <aedanl802@gmail.com>
2023-12-23 17:56:21 -0500

    added requestrr to remove files

--- bash/remove_files+containers.sh
+++ bash/remove_files+containers.sh
@@ -3,5 +3,5 @@
 rm -r /mc_server
 rm -r /media_server
 
-docker stop portainer mc prowlarr radarr sonarr qbittorrent emby
-docker rm portainer mc prowlarr radarr sonarr qbittorrent emby
+docker stop portainer mc prowlarr radarr sonarr requestrr qbittorrent emby
+docker rm portainer mc prowlarr radarr sonarr requestrr qbittorrent emby
