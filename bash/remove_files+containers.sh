2422a728175d10ee85b98c33cc79b52c74af4769
Aedan <aedanl802@gmail.com>
2023-12-24 17:57:48 -0500

    updated remove files script with purge command as well

--- bash/remove_files+containers.sh
+++ bash/remove_files+containers.sh
@@ -3,5 +3,7 @@
 rm -r /mc_server
 rm -r /media_server
 
-docker stop portainer mc prowlarr radarr sonarr requestrr qbittorrent emby
-docker rm portainer mc prowlarr radarr sonarr requestrr qbittorrent emby
+docker stop portainer mc prowlarr radarr sonarr requestrr qbittorrent emby filecloud.server filecould.mongodb filecloud.preview
+docker rm portainer mc prowlarr radarr sonarr requestrr qbittorrent emby filecloud.server filecould.mongodb filecloud.preview
+
+docker system purge -a
